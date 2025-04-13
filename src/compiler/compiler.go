7e5bc422a750909585c0b65c183d5f77a072164c
Anish Singla <singlaanish56@gmail.com>
2025-04-13 11:35:34 -0700

    compiler works with the first instruction wooooohoooo

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -24,12 +24,59 @@ func New() *Compiler{
 }
 
 func (c *Compiler) Compile(node ast.ASTNode) error{
+	switch node := node.(type){
+	case *ast.AstRootNode:
+		for _, statement := range node.Statements{
+			err := c.Compile(statement)
+			if err !=nil{
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil{
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil{
+			return err
+		}
+
+		err  =c.Compile(node.Right)
+		if err != nil{
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integerObject := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integerObject))
+
+	}
+
 	return nil
 }
-
+	
 func (c *Compiler) Bytecode() *Bytecode{
 	return &Bytecode{
 		Instructions: c.instructions,
 		Constants: c.constants,
 	}
+}
+
+//returns the index of the last instructions that is  set in the list
+func (c *Compiler) emit(operation code.Opcode, operands... int) int{
+	ins := code.Make(operation, operands...)
+	lastInstructionPos := c.addInstruction(ins)
+	return lastInstructionPos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int{
+	currentInstructionLen := len(c.instructions)
+	c.instructions= append(c.instructions, ins...)
+	return currentInstructionLen	
+}
+
+func (c *Compiler) addConstant(object object.Object) int{
+	c.constants=append(c.constants, object)
+	return len(c.constants) -1
 }
